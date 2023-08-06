dc8898f30ddeb7ce8abb1cd56643c9de0df0e27e
jessicahattig <jessicahattig@gmail.com>
2023-08-06 14:50:24 -0700

    WIP: Function 1, test 2. Green phase.

--- src/bike.js
+++ src/bike.js
@@ -1,7 +1,6 @@
-
 export default class Bike{
-  static getStolen(location) {
-    return fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen&access_token=${process.env.API_KEY}`)
+  static async getStolen(location) {
+    return fetch (`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen&access_token=${process.env.API_KEY}`)
     .then(function(response) {
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
