379a662f365aeca883bf7965300ca4faafdb68e7
jessicahattig <jessicahattig@gmail.com>
2023-08-06 11:51:12 -0700

    Function 1, test 2, good fail. Red phase.

--- __tests__/bike.test.js
+++ __tests__/bike.test.js
@@ -1,8 +1,14 @@
-import { Bike } from "../src/bike.js";
+import Bike from "../src/bike.js";
 
 describe("Bike",()=>{
   test ("Should create a Bike class", ()=> {
     const bike = new Bike();
     expect(bike).toEqual({})
+  });
+
+  test('Should search for bike when location is inputted', () => {
+    const bike = new Bike(Portland);
+    const mySearch = bike.getStolen();
+    expect(mySearch).toEqual(Portland);
   })
 })
