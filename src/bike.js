228f7047f75cebf33434a2482a18936547507b43
jessicahattig <jessicahattig@gmail.com>
2023-08-06 15:01:50 -0700

    Function 1, test 2, pass. Green phase.

--- src/bike.js
+++ src/bike.js
@@ -1,16 +1,16 @@
 export default class Bike{
   static async getStolen(location) {
-    return fetch (`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen&access_token=${process.env.API_KEY}`)
-    .then(function(response) {
+    try {
+      const response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen&access_token=${process.env.API_KEY}`);
+      const jsonifiedResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText}`;
+        const errorMessage = `${response.status} ${response.statusText}
+        ${jsonifiedResponse.message}`;
         throw new Error(errorMessage);
-      } else {
-        return response.json();
       }
-    })      
-    .catch(function(error) {
-      throw error;
-    });
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
   }
 }
