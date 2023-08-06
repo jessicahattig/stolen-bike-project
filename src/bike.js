10b90411fa50849c3c60a570a55121e949189989
jessicahattig <jessicahattig@gmail.com>
2023-08-06 12:27:18 -0700

    WIP: Function 1, test 2. Green phase.

--- src/bike.js
+++ src/bike.js
@@ -3,3 +3,25 @@ export default class Bike{
 
   }
 }
+  static getStolen(location) {
+    return new Promise(function (resolve, rejct) {
+      let request = new XXLHttpRequest();
+      const url ='https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=lKFRYom4Dut4bxZ_LxDRRWKxO9j51Kzt9mP02xRXhMg';
+      request.addEventListener("loanded", function(){
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200){
+          resolve ([response, city])
+        }else {
+          PromiseRejectionEvent([this,response, city]);
+        }
+        });
+        request.open("GET", url, true);
+        request.send();
+      })
+      
+    })
+  }
+    return fetch('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=lKFRYom4Dut4bxZ_LxDRRWKxO9j51Kzt9mP02xRXhMg');
+    .then(function(response) {
+      console.log(responses);
+    });
