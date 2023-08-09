bd0c45ec11150f0fb159234ff7971e1795783238
jessicahattig <jessicahattig@gmail.com>
2023-08-08 20:19:02 -0700

    WIP: Error handling.

--- src/bike.js
+++ src/bike.js
@@ -1,14 +1,14 @@
 export default class Bike {
   static async getStolen(location) {
     try {
-      // const response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=proximity&access_token=${process.env.API_KEY}`);
-      const response = await fetch(`https://bikeindex.org:443/api/v3/`);
-      console.log(location)
-      console.log(response)
+      const response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=proximity&access_token=${process.env.API_KEY}`);
+      // console.log(location)
+      // console.log(response)
       const jsonifiedResponse = await response.json();
+      console.log(jsonifiedResponse, location)
       if (!response.ok) {
-        const errorMessage = `${response.status} ${response.url}
-        ${jsonifiedResponse.message}`;
+        const errorMessage = `${response.status}. 
+        ${jsonifiedResponse.error}`;
         throw new Error(errorMessage);
       }
       return jsonifiedResponse;
